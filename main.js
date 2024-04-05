c45b4a8004cef82277f7db987b1153a8ced3fc84
SoyAlguien <108830157+SoyAlguien@users.noreply.github.com>
2024-04-05 10:18:15 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 var paddle2 = 10, paddle1 = 10;
 
 var paddle1X = 10, paddle1Height = 110;
-var paddle2Y = 685, paddle2Height = 70;
+var paddle2Y = 685, paddle2Height = 110;
 
 var score1 = 0, score2 = 0;
 var paddle1Y=0;
@@ -19,7 +19,7 @@ var paddle2y=0
 var ball = {
   x: 350 / 2,
   y: 480 / 2,
-  r: 20,
+  r: 10,
   dx: 3,
   dy: 3
 }
@@ -33,13 +33,21 @@ function setup() {
   coordenadas = ml5.poseNet(video, listo)
   //coordenadas.on("pose", respuesta)
   fondo=createImg("background1.jpg")
+  fondo.size(windowWidth,windowHeight)
   fondo.position(0,0)
+  rectMode(CENTER)
+  ellipseMode(RADIUS)
   nombre = createInput("").attribute("placeholder","Ingresa tu nombre")
   nombre.position(width/2,height/2)
+  nombre.center("horizontal")
+  nuevo = createButton("Nuevo Juego")
+  nuevo.position(windowWidth-200,windowHeight-100)
+  nuevo.mousePressed(limpiar)
   enviar=createButton("Ingresar")
   enviar.position(width/2,height/2+100)
+  enviar.center("horizontal")
   h2=createElement("h2")
-  h2.position(width/2,height/2)
+  h2.center()
   enviar.mousePressed(Ingresar)
   firebase.database().ref("estatus").on("value",(datos)=>{
     estatus=datos.val()
@@ -63,26 +71,16 @@ function respuesta(poses) {
 }
 function draw() {
 if(estatus==1){
-  if(!localStorage.getItem("jugador2")){
-    firebase.database().ref("jugadores").once("value",(datos)=>{
-        datos.forEach(function (reg) {
-          childKey = reg.key;
-          if (childKey != localStorage.getItem("jugador1")) {
-                localStorage.setItem("jugador2", childKey)
-                console.log(localStorage.getItem("jugador2"));
-                firebase.database().ref("jugadores/"+localStorage.getItem("jugador2")).on("value",(data2)=>{
-                  paddle2y = data2.val().y  - paddle2Height / 2
-            })
-          }
-        });
-  });
-}
   enviar.hide()
   nombre.hide()
   h2.hide()
   fondo.hide()
   background(0);
-  image(video, 0, 0, 350, 480)
+  if(localStorage.getItem("raqueta") == "jugador1"){
+    image(video, 0, 0, 350, 480)
+  }else{
+    image(video, 350, 0, 350, 480)
+  }
   fill("black");
   stroke("black");
   rect(680, 0, 20, 700);
@@ -99,12 +97,10 @@ if(estatus==1){
   stroke(0, 0, 250);
   strokeWeight(0.5);
   //paddle1Y = manoY;
-  firebase.database().ref("jugadores/"+localStorage.getItem("jugador1")).update({
+  firebase.database().ref("jugadores/"+localStorage.getItem("raqueta")).update({
     y: mouseY
   })
   rect(paddle1X, paddle1Y, paddle1, paddle1Height, 100);
-  rectMode(CENTER)
-
   //paleta de la computadora
   fill("#FFA500");
   stroke("#FFA500");
@@ -122,11 +118,16 @@ if(estatus==1){
 
   //llamar a la función move que es muy importante
   move();
-}else{
-  enviar.show()
-  nombre.show()
+}else if(localStorage.getItem("raqueta")){
   h2.show()
   fondo.show()
+  enviar.hide()
+  nombre.hide()
+}else{
+  h2.hide()
+  fondo.show()
+  enviar.show()
+  nombre.show()
 }
 }
 
@@ -134,11 +135,12 @@ if(estatus==1){
 
 //la función reset cuando la pelota no haga contacto con la paleta
 function reset() {
-  ball.x = width / 2 + 100,
-    ball.y = height / 2 + 100;
-  ball.dx = 3;
-  ball.dy = 3;
-
+  firebase.database().ref("pelota").update({
+    x:width / 2 + 100,
+    y:height / 2 + 100,
+    dx:3,
+    dy:3
+  })
 }
 
 
@@ -159,63 +161,74 @@ function drawScore() {
   textSize(20);
   fill("white");
   stroke(250, 0, 0)
-  text("Jugador:", 100, 50)
-  text(playerscore, 140, 50);
-  text("Computadora:", 500, 50)
-  text(pcscore, 555, 50)
+  text("Jugador 1:", 100, 50)
+  text(playerscore, 160, 50);
+  text("Jugador 2:", 500, 50)
+  text(pcscore, 560, 50)
 }
 
 
 //una función muy importante de este juego
 function move() {
+
   fill(50, 350, 0);
   stroke(255, 0, 0);
   strokeWeight(0.5);
-  ellipse(ball.x, ball.y, ball.r, 20)
-  ball.x = ball.x + ball.dx;
-  ball.y = ball.y + ball.dy;
-  if (ball.x + ball.r > width - ball.r / 2) {
-    ball.dx = -ball.dx - 0.5;
-  }
-  if (ball.x - 2.5 * ball.r / 2 < 0) {
-    if (ball.y >= paddle1Y && ball.y <= paddle1Y + paddle1Height) {
-      ball.dx = -ball.dx + 0.5;
-      playerscore++;
-    //sonido.play()
-      firebase.database().ref("pelota").update({
-        x:ball.x + ball.dx,
-        y:ball.y + ball.dy,
-        dx: ball.dx,
-        dy: ball.dy
+  ellipse(ball.x, ball.y, ball.r, ball.r)
+
+  firebase.database().ref("pelota").update({
+    x:ball.x + ball.dx,
+    y:ball.y + ball.dy
+  })
+  if (ball.x > paddle2Y - ball.r) {
+    if (ball.y >= paddle2y - paddle2Height/2 && ball.y <= paddle2y + paddle2Height/2) {
+    firebase.database().ref("pelota").update({
+      dx:-(ball.dx + 0.5)
+    })
+    firebase.database().ref("jugadores/jugador2").update({
+      puntos: pcscore++
+    })
+    }else{
+      firebase.database().ref("jugadores/jugador1").update({
+        puntos: playerscore++
       })
+      reset();
+    }
+  }
+  if (ball.x < paddle1X + ball.r) {
+    if (ball.y >= paddle1Y - paddle1Height && ball.y <= paddle1Y + paddle1Height) {
+      firebase.database().ref("pelota").update({
+        dx:-(ball.dx + 0.5)
+      })
+      firebase.database().ref("jugadores/jugador1").update({
+        puntos: playerscore++
+      })
+    //sonido.play()
     }
     else {
-      pcscore++;
+      firebase.database().ref("jugadores/jugador2").update({
+        puntos: pcscore++
+      })
       reset();
       //navigator.vibrate(100);
     }
   }
   if (ball.y + ball.r > height || ball.y - ball.r < 0) {
     //sonido.play()
-    ball.dy = - ball.dy;
     firebase.database().ref("pelota").update({
-      x:ball.x + ball.dx,
-      y:ball.y + ball.dy,
-      dx: ball.dx,
-      dy: ball.dy
+      dy: - ball.dy
     })
   }
-  if (pcscore == 4) {
+  if (pcscore >= 4 || playerscore >=4) {
     fill("#FFA500");
     stroke(0)
-    rect(0, 0, width, height - 1);
+    rect(width*0.5, height*0.5, width, height);
     fill("white");
     stroke("white");
     textSize(25)
     text("¡Fin del juego!☹☹", width / 2, height / 2);
     text("¡Volver a cargar la página!", width / 2, height / 2 + 30)
     noLoop();
-    pcscore = 0;
   }
 }
 
@@ -225,39 +238,69 @@ function models() {
   textSize(18);
   fill(255);
   noStroke();
-  text("Ancho:" + width, 135, 15);
-  text("Velocidad:" + abs(ball.dx), 50, 15);
-  text("Altura:" + height, 235, 15)
+  text("Ancho: " + width, 190, 15);
+  text("Velocidad: " + abs(ball.dx), 70, 15);
+  text("Altura: " + height, 300, 15)
 }
 function Ingresar(){
   enviar.hide()
   nombre.hide()
   h2.html("Espera a que cada jugador se una")
-  firebase.database().ref("jugadores/"+nombre.value()).set({
-    nombre:nombre.value(),
-    x:0,
-    y:0,
-    puntos:0
-  })
   firebase.database().ref("jugadores").once("value",(datos)=>{
-    if (datos.numChildren()===2){
+    if(datos.numChildren()===0){
+      localStorage.setItem("jugador1", nombre.value());
+      localStorage.setItem("raqueta", "jugador1");
+      firebase.database().ref("jugadores/jugador1").set({
+        nombre:nombre.value(),
+        x:10,
+        y:350,
+        puntos:0
+      })
+      firebase.database().ref("jugadores").on("value",(datos)=>{
+        paddle1Y = datos.val()["jugador1"].y
+        paddle2y = datos.val()["jugador2"].y
+        playerscore = datos.val()["jugador1"].puntos
+        pcscore = datos.val()["jugador2"].puntos
+      })
+    }
+    if (datos.numChildren()===1){
+      localStorage.setItem("jugador1", datos.val()["jugador1"]["nombre"]);
+      localStorage.setItem("jugador2", nombre.value());
+      localStorage.setItem("raqueta", "jugador2");
+      firebase.database().ref("jugadores/jugador2").set({
+        nombre:nombre.value(),
+        x:685,
+        y:350,
+        puntos:0
+      })
       h2.hide()
       fondo.hide()
       firebase.database().ref("/").update({estatus: 1})
+      reset()
+      firebase.database().ref("jugadores").on("value",(datos)=>{
+        paddle1Y = datos.val()["jugador1"].y
+        paddle2y = datos.val()["jugador2"].y
+        playerscore = datos.val()["jugador1"].puntos
+        pcscore = datos.val()["jugador2"].puntos
+      })
     }
-  })
-  localStorage.setItem("jugador1", nombre.value());
-  firebase.database().ref("jugadores/"+localStorage.getItem("jugador1")).on("value",(data)=>{
-    paddle1Y = data.val().y
   })
 }
 
 //esta función ayuda a que la paleta no salga del canvas
 function paddleInCanvas() {
-  if (mouseY + paddle1Height > height) {
-    mouseY = height - paddle1Height;
+  if (mouseY - paddle1Height/2 > height) {
+    mouseY = height - paddle1Height/2;
   }
-  if (mouseY < 0) {
-    mouseY = 0;
+  if (mouseY + paddle1Height/2 < 0) {
+    mouseY = paddle1Height/2;
   }
+}
+function limpiar(){
+  firebase.database().ref("/").update({
+    estatus:0,
+    jugadores:null
+  })
+  localStorage.clear()
+  window.location.reload();
 }
